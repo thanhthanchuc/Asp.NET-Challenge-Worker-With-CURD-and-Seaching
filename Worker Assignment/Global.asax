21bac3f969788aa2792147d2a68ee0ac7c9f005b
toiye <toiye@DESKTOP-BJB9GPG>
2018-08-01 01:17:44 +0700

    Add project files.

---
+++ Worker Assignment/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="Worker_Assignment.MvcApplication" Language="C#" %>
---
+++ Worker Assignment/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(Worker_Assignment.Startup))]
+namespace Worker_Assignment
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
